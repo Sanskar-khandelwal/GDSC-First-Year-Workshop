7969a8aa7b03364ccc92ecbf67e2eba2dbc0c6fa
Sanskar khandelwal <Sanskarkhandelwal336@gmail.com>
2023-01-07 22:06:34 +0530

    added the starter file

---
+++ Starter/script.js
@@ -0,0 +1,6 @@
+const body = document.querySelector("body");
+const nav = document.querySelector("nav");
+const btn = document.querySelector("#btn-change");
+const home = document.querySelector("#home");
+const about = document.querySelector("#about");
+const contactUs = document.querySelector("#contact-us");
